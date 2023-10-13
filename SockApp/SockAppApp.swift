10e10a6903e1c32818544faaff6c3ecb7ecb9812
chunta-chen <chunta.chen@lookout.com>
2023-10-13 15:08:37 +0800

    first commit

---
+++ SockApp/SockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SockAppApp.swift
+//  SockApp
+//
+//  Created by ChunTa Chen on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
